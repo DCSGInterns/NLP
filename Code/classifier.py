1883cd7e76fd0a0bf4c617b6496a79bc7ddb0e39
thankachankarun639 <karunthankachan1@gmail.com>
2014-06-15 19:20:27 +0530

    Slight changes to tokenizer
    made available more options for function, must test out mroe abbrev and
    such

--- Code/classifier.py
+++ Code/classifier.py
@@ -1,6 +1,7 @@
 ## TOKENIZING
 
-"""para = ''' This is a test paragraph. The idea is to find out all the ways in
+#para  = raw_input("Comment Here : ")
+para = ''' This is a test paragraph. The idea is to find out all the ways in
 which our sentence tokenizer would fail -
 1. Hey!!!, What are you doing??
 2. THIS IS PRIVATE PROPERTY. HANDS OFFF!!!!!
@@ -9,46 +10,52 @@ which our sentence tokenizer would fail -
 4. Phone No : +91-94465-00359, 0484-2203982.
 5. Emoticons : :) :( :P.
 6. Ellipssis : .......
-7. Leinghter Text : SHHHHIITIIIIIITTTTTT!!!!!!''' 
+7. Leinghter Text : HECCKKKK NOOOOOOOO!!!!!!''' 
 
-"""
 def classifier():
-	para = raw_input("Comment here : ");
-	import nltk
-	sent_tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
-	sent_result = sent_tokenizer.tokenize(para)
-	#for sent in sent_result:
-	#    print sent
-	#    print "_______________________________"
+        import nltk
+        
+        #from nltk.tokenize.punkt import PunktSentenceTokenizer, PunktParameters
+        #punkt_param = PunktParameters()
+        #punkt_param.abbrev_types = set(['dr', 'vs', 'mr', 'mrs', 'prof', 'inc'])
+        #sentence_tokenizer = PunktSentenceTokenizer(punkt_param)
+        sent_tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
 
-	##this is to do tokenizing based on RE
-	from nltk.tokenize import RegexpTokenizer
+        sent_result = sent_tokenizer.tokenize(para)
+        #for sent in sent_result:
+        #    print sent
+        #    print "_______________________________"
+        
+        ##this is to do tokenizing based on RE
+        from nltk.tokenize import RegexpTokenizer
 
-	##to store the array of arrays
-	data_store=[]
+        emoticons = "[<>]?[:;=8][\-o\*\']?[\)\]\(\[dDpP/\:\}\{@\|]"
+        real_numbers = "[+\-]?\d+[0123456789,/.:-]*%?"
+        hashtags = "\#[\w_]+[\w\'_\-]*\w+"
+        all_words = "[\w'\-_]*\w[\w'\-_]*"
+        line_endings = "[.:;,!?]+"
 
-	emoticons = "[<>]?[:;=8][\-o\*\']?[\)\]\(\[dDpP/\:\}\{@\|]"
-	real_numbers = "[+\-]?\d+[0123456789,/.:-]*%?"
-	hashtags = "\#[\w_]+[\w\'_\-]*\w+"
-	all_words = "[\w'\-_]*\w[\w'\-_]*"
-	line_endings = "[.:;,!?]+"
+        reg_string = emoticons+"|"+real_numbers+"|"+hashtags+"|"+all_words+"|"+line_endings
+        word_tokenizer = RegexpTokenizer(reg_string)
 
-	reg_string = emoticons+"|"+real_numbers+"|"+hashtags+"|"+all_words+"|"+line_endings
-	word_tokenizer = RegexpTokenizer(reg_string)
+        ##to store the array of arrays
+        data_store=[]
 
-	for sent in sent_result :
-    	 word_result = word_tokenizer.tokenize(sent)
-    	 data_store.append(word_result)
+        for sent in sent_result :
+                word_result = word_tokenizer.tokenize(sent)
+                data_store.append(word_result)
     
-	return data_store
-
-
-
+        print data_store
+	#return data_store
+	
 ''' ISSUES WITH SENTENCE TOKENIZER
 ** Cannot recognize bullet types eg, 1. Sm text, 2. Some text (Mr. Mrs. Dr. Prof.)  - ITS POSSIBLE TO TRAIN
-** Splits multiple exclamation points into multiple sentences - WHY???
+** Splits multiple exclamation/question marks onto multiple sentences - WHY???
 '''
 
 ''' ISSUES WITH WORD TOKENIZER
-NOT SURE WHAT ALL COULD COME AS all_words - cud be possible that i missed out something >>> combination of words/numbers/symbols
+NOT SURE WHAT ALL COULD COME AS all_words - cud be possible that i missed out something >>> 
+combination of words/numbers/symbols???
 '''
+
+classifier()
