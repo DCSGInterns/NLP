008a4e232f6b725e408273752ed2288e3149f503
thankachankarun639 <karunthankachan1@gmail.com>
2014-07-25 10:42:58 +0530

    Updating Website
    Still bugs persist in Heat Map

--- Website/db/db_connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-$mysql_host = 'localhost';
-$mysql_user = 'InsightsUls0k';
-$mysql_password = '{2qGq(22+5iU';
-$mysql_error = 'Could not connect';
-$con = mysqli_connect($mysql_host,$mysql_user,$mysql_password,"Insights") or die($mysql_error);
-?>
-
--- Website/py_code/test.py
+++
@@ -1,5 +0,0 @@
-from Final import main_file
-
-sentiment = main_file.custom_SA("Dell", "realised why lid on my Dell vostro v131 has gone wrong - @dell didnt put screws in. many other missing screws found inside @dellcares ")
-
-print sentiment
